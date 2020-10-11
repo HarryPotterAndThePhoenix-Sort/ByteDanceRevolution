14d9272385f820ca930e4d438e24029168c98d45
Pawan Benjamin <pawanbenjamin@gmail.com>
2020-10-11 11:48:54 -0400

    feat: firebase get single user and audio

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -4,17 +4,37 @@ import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 import { AuthUserContext, withAuthorization } from '../Session'
 
-const AccountPage = () => (
-  <AuthUserContext.Consumer>
-    {authUser => (
-      <div>
-        <h1>Account: {authUser.email}</h1>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
-      </div>
-    )}
-  </AuthUserContext.Consumer>
-);
+class AccountPage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    const userId = this.props.firebase.auth.currentUser.uid
+    this.props.firebase.user(userId).on('value', snapshot => {
+      let user = snapshot.val()
+      this.setState({user})
+    })
+  }
+
+  render(){
+  return (
+    <AuthUserContext.Consumer>
+      {authUser => (
+        <div>
+          <h1>Username: {this.state.user.username}</h1>
+          <h1>Email: {this.state.user.email}</h1>
+          <PasswordForgetForm />
+          <PasswordChangeForm />
+        </div>
+      )}
+    </AuthUserContext.Consumer>
+   )
+  }
+};
 
 const condition = authUser => !!authUser;
 
--- src/components/Webcam/index.js
+++ src/components/Webcam/index.js
@@ -1,11 +1,23 @@
 import Webcam from 'react-webcam';
-import React, { useRef } from 'react';
-import * as tf from "@tensorflow/tfjs"
+import React, { useEffect, useRef, useState } from 'react';
 import * as posenet from "@tensorflow-models/posenet"
 import { drawKeypoints, drawSkeleton } from './utilities'
+import { withAuthorization } from '../Session';
+import Audio from '../Audio'
 
 
-function WebcamComponent() {
+function WebcamComponent(props) {
+  const [user, setUser] = useState(null)
+
+  console.log(user)
+  useEffect(()=>{
+    const userId = props.firebase.auth.currentUser.uid
+    props.firebase.user(userId).on('value', snapshot => {
+      let user = snapshot.val()
+      setUser(user)
+    })
+  }, [])
+
   const webcamRef = useRef(null)
   const canvasRef = useRef(null)
 
@@ -38,42 +50,42 @@ function WebcamComponent() {
       return pose
       // console.log(pose)
 
-      
+
 
     }
   }
 
-  
+
   async function makeVectors () {
     let vector = await runPosenet()
     let result = []
     vector.keypoints.map(position => {
       result.push(position.position.x, position.position.y)
     })
-    
+
     vector.keypoints.map(position => {
       result.push(position.score)
     })
-    
+
     result.push(vector.score)
     console.log('VECTOR:', vector, 'RESULT:', result)
     return result
-    
-    
+
+
   }
-  
+
   const vector2 = [296.79050677652106, 277.61852748180877, 357.6791024765991, 231.86998014390593, 258.28329148790954, 224.40099767736487, 439.23511237324493, 266.43424749870064, 219.53972065132606, 239.4013935810811, 482.07732059282375, 445.9070560193607, 167.19542219188767, 366.9290154788202, 65.51942917560453, 275.28388326728174, 143.64909869832294, 217.27745643028848, 96.35863305625975, 222.88339214364606, 88.64931198810453, 220.79961384160603, 115.62231102525351, 260.54231703157484, 113.0885655348089, 482.4794536122661, 136.17030579660687, 210.29471986259097, 132.17008641283152, 208.05719387506497, 128.1816893769501, 476.46278302040025, 112.82647809818643, 237.8032520952443, 0.9507791996002197, 0.9095539450645447, 0.9860280752182007, 0.13406424224376678, 0.6685985922813416, 0.061165839433670044, 0.6769523620605469, 0.022768331691622734, 0.061707671731710434, 0.07040218263864517, 0.09334135800600052, 0.009731590747833252, 0.014465742744505405, 0.019178742542862892, 0.03679882735013962, 0.02329391799867153, 0.017492735758423805, 0.2797837268889827]
-  
+
   function weightedDistanceMatching(poseVector1, poseVector2) {
     let vector1PoseXY = poseVector1.slice(0, 34);
     let vector1Confidences = poseVector1.slice(34, 51);
     let vector1ConfidenceSum = poseVector1.slice(51, 52);
-    
+
     let vector2PoseXY = poseVector2.slice(0, 34);
-    
+
     // First summation
     let summation1 = 1 / vector1ConfidenceSum;
-    
+
     // Second summation
     let summation2 = 0;
     for (let i = 0; i < vector1PoseXY.length; i++) {
@@ -81,22 +93,28 @@ function WebcamComponent() {
       let tempSum = vector1Confidences[tempConf] * Math.abs(vector1PoseXY[i] - vector2PoseXY[i]);
       summation2 = summation2 + tempSum;
     }
-    
+
     return summation1 * summation2;
   }
-  
+
   const handleClick = async (event, bpm) => {
 
     event.preventDefault()
-    
+
+    const audio = document.getElementsByClassName("audio-element")[0]
+    console.log(audio)
+    await audio.play()
+
     setInterval(async ()=> {
       const vector = await makeVectors()
       console.log(weightedDistanceMatching(vector, vector2))
-      
-    }, 8000) 
-    
+
+    }, 8000)
+
 
   }
+
+
 
   const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
     const ctx = canvas.current.getContext("2d")
@@ -106,10 +124,18 @@ function WebcamComponent() {
     drawKeypoints(pose["keypoints"], 0.5, ctx)
     drawSkeleton(pose["keypoints"], 0.5, ctx)
   }
-  
+
   return (
     <div>
        <header>
+       <h3>User Name: {user !== null ? user.username : null}</h3>
+       <Audio />
+       <div>
+         <span className='metronone'>.</span>
+         <span className='metronone'>.</span>
+         <span className='metronone'>.</span>
+         <span className='metronone'>.</span>
+       </div>
         <Webcam
           ref={webcamRef}
           style={{
@@ -147,7 +173,11 @@ function WebcamComponent() {
 
 }
 
-export default WebcamComponent
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(WebcamComponent);
+
+
 
 //{score: 0.2703212749706033, keypoints: Array(17)}
 // keypoints: Array(17)
