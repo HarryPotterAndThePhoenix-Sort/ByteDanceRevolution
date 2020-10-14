bdca628d6175b0394d77bd88bd7855c5a53900bf
Namratha <namrathapmd@gmail.com>
2020-10-14 15:19:21 -0500

    CHORE: Fixing timing between poses and PoseNet

--- src/components/Webcam/pose.js
+++ src/components/Webcam/pose.js
@@ -1,49 +1,58 @@
-import React, { useState } from 'react'
-import poseData from './poseData'
+import React, { useState, useEffect } from "react";
+import poseData from "./poseData";
+//import DancePoses from "../../DancePoses";
+import img1 from "../../DancePoses/1.jpg";
+import img2 from "../../DancePoses/2.jpg";
+import img3 from "../../DancePoses/3.jpg";
+import img4 from "../../DancePoses/4.jpg";
 
-function PoseOverlay () {
-  let [image, setImage] = useState(1)
-
-  const test = () => {
-
-    const changeBackgroundImage = setInterval(async () => {
-      let background = document.getElementById(image)
-      setImage(image === 4 ? 1: image++)
-      return background
-    }, 2000);
-
+export default class PoseOverlay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //images: [img1, img2, img3, img4],
+      image: 1,
+    };
   }
 
+  componentDidMount() {
+    const changeBackgroundImage = setInterval(async () => {
+      if (this.state.image < 5) this.setState({ image: this.state.image + 1 });
+      else clearInterval(changeBackgroundImage);
+    }, 3500);
+  }
 
-    
+  componentWillUnmount() {
+    this.setState({ image: null });
+  }
 
-
+  render() {
+    console.log("Image index", this.state.image);
     return (
-        <div>
-           <header>
-            <video
-              style={{
-                display: "flex",
-                alignContent: "center",
-                position: "absolute",
-                marginLeft: "auto",
-                marginRight: "auto",
-                left: 0,
-                right: 0,
-                textAlign: "center",
-                zindex: -1,
-                opacity: 0.5,
-                width: 640,
-                height: 480,
-                backgroundColor: "violet",
-                backgroundImage: "url('./ymca.gif')",
-                backgroundPosition: "center"
-              }}
-            />
-          </header>
-        </div>
-    )
-
+      <div>
+        <header>
+          <video
+            style={{
+              display: "flex",
+              alignContent: "center",
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: -1,
+              opacity: 0.5,
+              width: 640,
+              height: 480,
+              backgroundColor: "violet",
+              backgroundImage: `url('./${this.state.image}.jpg')`,
+              backgroundPosition: "center",
+            }}
+          />
+          {/* <img src={"./1.jpg"} /> */}
+        </header>
+      </div>
+    );
+  }
 }
-
-export default PoseOverlay
